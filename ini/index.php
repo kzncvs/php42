d6837f59d97f8ed2c159c1df1a8c8b3fac39dddf
Sergey Kazancev <s@kazancev.xyz>
2018-04-09 16:06:45 +0300

    Completed ini task

--- ini/index.php
+++ ini/index.php
@@ -1,10 +1,9 @@
 <?php
 define("INI_FILE_NAME", 'index.ini');
 
-$ini_array = parse_ini_file(INI_FILE_NAME, true);
-print_r($ini_array);
 
-$file_for_edit = $ini_array['main']['filename'];
+$ini_array = parse_ini_file(INI_FILE_NAME, true);
+
 
 $first_rule_seq = $ini_array['first_rule']['symbol'];
 $first_rule_upper = $ini_array['first_rule']['upper'];
@@ -12,9 +11,51 @@ $first_rule_upper = $ini_array['first_rule']['upper'];
 $second_rule_seq = $ini_array['second_rule']['symbol'];
 $second_rule_direction = $ini_array['second_rule']['direction'];
 
-$third_rule_seq = $ini_array['second_rule']['symbol'];
-$second_rule_direction = $ini_array['second_rule']['delete'];
+$third_rule_seq = $ini_array['third_rule']['symbol'];
+$third_rule_delete = $ini_array['third_rule']['delete'];
+
+$file_link = $ini_array['main']['filename'];
+$file_lines = file($file_link);
 
 
-
-
+foreach ($file_lines as $line) {
+    if (strpos($line, $first_rule_seq) === 0) {
+        if ($first_rule_upper) {
+            echo strtoupper($line) . '<br>';
+        } else {
+            echo strtolower($line) . '<br>';
+        }
+    } else if (strpos($line, $second_rule_seq) === 0) {
+        for ($i = 0; $i < strlen($line); $i++) {
+            $char = $line[$i];
+            if (is_numeric($char)) {
+                if ($second_rule_direction == '+') {
+                    if ($char >= 0 and $char < 9) {
+                        echo $char + 1;
+                    } else {
+                        echo '0';
+                    }
+                } else if ($second_rule_direction == '-') {
+                    if ($char <= 9 and $char > 0) {
+                        echo $char - 1;
+                    } else {
+                        echo '9';
+                    }
+                }
+            } else {
+                echo $char;
+            }
+        }
+        echo '<br>';
+    } else if (strpos($line, $third_rule_seq) === 0) {
+        for ($i = 0; $i < strlen($line); $i++) {
+            $char = $line[$i];
+            if ($char != $third_rule_delete) {
+                echo $char;
+            }
+        }
+        echo '<br>';
+    } else {
+        echo $line . '<br>';
+    }
+}
